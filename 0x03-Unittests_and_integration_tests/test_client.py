79666e21e6b360b284ce679c6d886ab2ec03de18
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-08-26 22:50:54 +0300

    Task 08 & 09

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import unittest
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from unittest.mock import patch, MagicMock, PropertyMock
 from client import GithubOrgClient
 from typing import Dict
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -63,3 +64,50 @@ class TestGithubOrgClient(unittest.TestCase):
         """Tests the method has_license."""
         output = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(output, expected)
+
+
+@parameterized_class(
+    [
+        {
+            "org_payload": TEST_PAYLOAD[0][0],
+            "repos_payload": TEST_PAYLOAD[0][1],
+            "expected_repos": TEST_PAYLOAD[0][2],
+            "apache2_repos": TEST_PAYLOAD[0][3],
+        }
+    ]
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Test suite for public_repos."""
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Sets up the fixtures."""
+        cls.get_patcher = patch('requests.get')
+        cls.mock_get = cls.get_patcher.start()
+
+        def mock_requests_get(url, *args, **kwargs):
+            if url.endswith('orgs/test-org'):
+                return MagicMock(json=lambda: cls.org_payload)
+            elif url.endswith('repos'):
+                return MagicMock(json=lambda: cls.repos_payload)
+            raise ValueError("Unexpected URL")
+
+        cls.mock_get.side_effect = mock_requests_get
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Removes the fixtures."""
+        cls.get_patcher.stop()
+
+    def test_public_repos(self) -> None:
+        """Tests the method public_repos."""
+        self.assertEqual(
+            GithubOrgClient('test-org').public_repos(),
+            self.expected_repos
+        )
+
+    def test_public_repos_with_license(self) -> None:
+        """Tests the method public_repos with arg: license="apache-2.0."""
+        self.assertEqual(
+            GithubOrgClient('test-org').public_repos(license="apache-2.0"),
+            self.apache2_repos
+        )
