2ecd4dc125522a5327045ae31c6c6ef9a6a2f797
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-08-27 13:58:49 +0300

    Task 07

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -61,7 +61,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False),
         ])
-    def test_license(self, repo: Dict, license_key: str, expected: bool):
+    def test_has_license(self, repo: Dict, license_key: str, expected: bool):
         """Tests the method has_license."""
         output = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(output, expected)
