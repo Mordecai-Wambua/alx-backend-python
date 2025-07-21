963f2ab6a8883c8658dcd1da7f5564a8acc46a16
Mordecai-Wambua <mordecaingumbau@gmail.com>
2025-07-21 10:59:29 +0300

    Parameterize a unit test task 01

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,17 +5,32 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """Unit tests for the access_nested_map function"""
-    @parameterized.expand([
-        ({"a": 1}, ["a"], 1),
-        ({"a": {"b": 2}}, ["a"], {"b": 2}),
-        ({"a": {"b": 2}}, ["a", "b"], 2),
-    ])
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ["a"], 1),
+            ({"a": {"b": 2}}, ["a"], {"b": 2}),
+            ({"a": {"b": 2}}, ["a", "b"], 2),
+        ]
+    )
     def test_access_nested_map(self, nested_map, path, expected_value):
         """Test successful retrieval from nested map."""
         self.assertEqual(access_nested_map(nested_map, path), expected_value)
 
+    @parameterized.expand(
+        [
+            ({}, ["a"], "a"),
+            ({"a": 1}, ["a", "b"], "b"),
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path, exception_message):
+        """Test that KeyError is raised with correct missing key message"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{exception_message}'")
 
 
 if __name__ == "__main__":
