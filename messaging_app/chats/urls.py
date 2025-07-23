5d463404685cb0d066319e481566c640c17e3ff2
Mordecai-Wambua <mordecaingumbau@gmail.com>
2025-07-23 12:10:55 +0300

    fix

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from rest_framework_nested.routers import NestedDefaultRouter
+from rest_framework_nested import routers
 
 from .views import ConversationViewSet, MessageViewSet, RegisterUserView, LoginView
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversations")
 router.register(r"messages", MessageViewSet, basename="messages")
 
 # Creates nested routes under conversations.
-conversation_router = NestedDefaultRouter(router, r"conversations", lookup="conversation")
+conversation_router = routers.NestedDefaultRouter(router, r"conversations", lookup="conversation")
 conversation_router.register(r"messages", MessageViewSet, basename="conversation-messages")
 
 urlpatterns = [
