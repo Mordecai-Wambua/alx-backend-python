05d6f3e6682f8ac06783d5bcdf89a8f147df13aa
Mordecai-Wambua <mordecaingumbau@gmail.com>
2025-07-27 23:40:17 +0300

    checker

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -41,6 +41,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "chats.middleware.RequestLoggingMiddleware",
+    "chats.middleware.RestrictAccessByTimeMiddleware",
+    "chats.middleware.OffensiveLanguageMiddleware",
+    "chats.middleware.RolepermissionMiddleware",
 ]
 
 ROOT_URLCONF = "messaging_app.urls"
